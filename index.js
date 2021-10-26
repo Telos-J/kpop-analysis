1b7c8f534b678ba103e0774ad166c33b8c55e46d
Jiseung Jung <roadrunner23@yonsei.ac.kr>
2021-10-27 01:16:34 +0900

    only use dotenv for development

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const express = require('express')
 const path = require('path')
 const indexRouter = require('./routes/index')
 const twitterRouter = require('./routes/twitter')
-require("dotenv").config();
-
-const port = process.env.PORT || 5000
 const app = express()
 
-if (process.env.NODE_ENV === "production")
+if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "client/build")));
+} else {
+    require("dotenv").config();
+}
+
+const port = process.env.PORT || 5000
 
 app.use('/twitter', twitterRouter)
 app.use('/', indexRouter)
