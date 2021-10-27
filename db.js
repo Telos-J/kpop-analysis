bf896a283104f94e4268e5764e223f87f4432852
Jiseung Jung <roadrunner23@yonsei.ac.kr>
2021-10-27 16:43:26 +0900

    set ssl to true

--- db.js
+++ db.js
@@ -1,15 +1,16 @@
 const { Pool } = require('pg')
 
 const devConfig = {
-	user: process.env.PG_USER,
-	password: process.env.PG_PASSWORD,
-	host: process.env.PG_HOST,
-	database: process.env.PG_DATABASE,
-	port: process.env.PG_PORT,
+    user: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    port: process.env.PG_PORT,
 };
 
 const proConfig = {
-    connectionString: process.env.DATABASE_URL
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
 }
 
 const pool = new Pool(
